74257dce2a7f649f7a4c1439bb541846e145aa9e
Bo <anvu0494@gmail.com>
2021-04-07 20:41:33 -0400

    adding swagger2 documentation

--- src/main/java/com/sjl/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/sjl/employeemanagement/controller/EmployeeController.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sjl.employeemanagement.entities.Employee;
 import com.sjl.employeemanagement.repository.EmployeeRepository;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 	private final EmployeeRepository employeeRepository;
 
@@ -21,22 +25,29 @@ public class EmployeeController {
 		this.employeeRepository = employeeRepository;
 	}
 
-	@GetMapping("/employees")
+	@GetMapping("/")
+	@ApiOperation(value = "creates a new employee")
 	public List<Employee> all() {
 		return employeeRepository.findAll();
 	}
 
-	@PostMapping("/employees")
+	@PostMapping("/")
+	@ApiOperation(value = "creates a new employee",
+		response =Employee.class)
 	public Employee newEmployee(@RequestBody Employee newEmployee) {
 		return employeeRepository.save(newEmployee);
 	}
 
-	@GetMapping("/employees/{id}")
+	@GetMapping("/{id}")
+	@ApiOperation(value = "returns one employee by the employee ID",
+			response =Employee.class)
 	public Employee one(@PathVariable Long id) {
 		return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 	}
 
-	@PutMapping("/employees/{id}")
+	@PutMapping("/{id}")
+	@ApiOperation(value = "updates an existing employee, if employee does not exist, it will create one",
+		response =Employee.class)
 	public Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 		return employeeRepository.findById(id).map(employee -> {
 			employee.setFirstName(newEmployee.getFirstName());
@@ -53,7 +64,8 @@ public class EmployeeController {
 		});
 	}
 
-	@DeleteMapping("/employees/{id}")
+	@DeleteMapping("/{id}")
+	@ApiOperation(value = "deletes an existing employee")
 	public void deleteEmployee(@PathVariable Long id) {
 		employeeRepository.deleteById(id);
 	}

9ab89acf5feb3238d482941171e930802cae75f4
Shenjie Lin <linshenjie321@gmail.com>
2021-04-20 10:51:35 -0400

    solved weird 404 405 error when returning error

--- src/main/java/com/sjl/employeemanagement/controller/ValidationExceptionHandler.java
+++ src/main/java/com/sjl/employeemanagement/controller/ValidationExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
@@ -15,6 +16,7 @@ public class ValidationExceptionHandler {
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseBody
 	public Map<String, String> handleValidationExceptions(
 	  MethodArgumentNotValidException ex) {
 	    Map<String, String> errors = new HashMap<>();

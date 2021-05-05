f90ac74f5772df50a69fbbef6c90a42097ce54c5
Shenjie Lin <linshenjie321@gmail.com>
2021-05-05 18:29:53 -0400

    adding some example for converting image to pdf using itext

--- src/main/java/com/sjl/pdfcustom/PdfUtil.java
+++ src/main/java/com/sjl/pdfcustom/PdfUtil.java
@@ -2,22 +2,56 @@ package com.sjl.pdfcustom;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.springframework.core.io.ClassPathResource;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class PdfUtil {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
+		pdfMergerUtilMerge();
+//		imageToPdf();
+	}
+	
+	private static void pdfMergerUtilMerge() throws IOException {
 		PDFMergerUtility pdfboxMerger = new PDFMergerUtility();
-		File file1 = new File("location absolute path");
-		File file2 = new File("location absolute path");
+		File file1 = new File("absolute location 1");
+		File file2 = new File("absolute location 2");
 		pdfboxMerger.addSource(file1);
 		pdfboxMerger.addSource(file2);
-		pdfboxMerger.setDestinationFileName("location absolute path");
+		pdfboxMerger.setDestinationFileName("absolute location destination");
 		pdfboxMerger.mergeDocuments();
+	}
+	
+	private static void iTextMerge() {
+		
+	}
+	
+	//note this one has image resizing issue, not sure how to resolve yet, but one should properly size the image first
+	private static void imageToPdf() {
+		Document document = new Document();
+		String input = "image absolute location";
+		String output = "pdf output absolute location";
+		try {
+			FileOutputStream fos = new FileOutputStream(output);
+			PdfWriter pdfWriter = PdfWriter.getInstance(document, fos);
+			pdfWriter.open();
+			document.open();
+			document.add(Image.getInstance(input));
+			document.close();
+			pdfWriter.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
